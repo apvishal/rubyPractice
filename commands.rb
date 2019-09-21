7cce15acc2a8ea69e41238620201b0b8f44f99ba
vp <vpatel@localhost.localdomain>
2019-09-20 18:30:06 -0700

    use maps for lists and commands

--- commands.rb
+++ commands.rb
@@ -1,4 +1,3 @@
-
 class ListInterpreter
   # public...
   def initialize()
@@ -7,7 +6,8 @@ class ListInterpreter
     @validCommands = ["declare", "undeclare", "print", "add", "remove", "clear", 
                         "size", "list", "test_1", "test_2", "test_3", "test_4", "upcase"]
     # hashmap to store all lists...
-    @listHash = Hash.new
+    @commandMap = Hash.new
+    @listMap = Hash.new
   end
   def getCommand
     return @command
@@ -16,52 +16,84 @@ class ListInterpreter
     @command = newCommand
   end
   def execute
-    # extract the command from the string ( if this is a valid command string, then the command will be the first word in the string)
-    currentCommand = @command.split(' ').first
-	# check if the currentCommand is a valid one
-    if ! verifyCommand(currentCommand)
+    # create a hashmap with the elements in the command string
+    populateHashMap()
+    # check if the command provided is a valid one
+    if ! verifyCommand(@commandMap["command"])
       return false
     end
-    # check if the command is 'upcase' or 'list'
-    if currentCommand.eql?("upcase") || currentCommand.eql?("list") || currentCommand.eql?("declare") 
-      return send(currentCommand)
-    end
-    # all leftover commands will need a existing list name, except declare
-    if ! doesListExist(@command.split(' ')[1])
-      return false
-    end
-    # call the appropriate function, return its value (we already checked if it is a valid command earlier with verifyCommand...)
-    commandArr = @command.split(' ',3)
-	
-    return send(currentCommand, commandArr[1], commandArr[2])
 
+    # if declare, and the list exists, return an error
+    # or is not declare, and list Doesnt exist, return error
+    if ((@commandMap["command"] =="declare" && doesListExist()) || ( !(@commandMap["command"] == "declare")) && !doesListExist())
+      if (@commandMap["command"] != "upcase" && @commandMap["command"] != "list" )
+        puts "invalid list name"
+        return false
+      end
+    end
+    
+    # call the appropriate function, return its value (we already checked if it is a valid command earlier with verifyCommand...)
+    if send("_" << @commandMap["command"])
+        clearCommand()
+        return true
+    end
+    return false
 
   end
   # private functions...
   private
-	def undeclare(listName, args)
-		puts "HELLO FFROM UNDECLARE"
-		puts listName
-		argsArr = args.split(',')
-		argsArr.each {|elem| puts elem.strip}
-	end
-	def declare
-		commandArr = @command.split(' ', 3)
-		if commandArr.size > 1
-			# we have multiple args...
-			if doesListExist
-	end
-    def doesListExist(someListName)
-		# eventually, this function will check a hashmap to see if someListName is a key in the map...
+    def populateHashMap()
+            # split the command into an array first, at most three elements...
+            commandArr = @command.split(' ', 3)
+            puts "LENGTH "
+            puts commandArr.length
+            # store the first element as the command
+            @commandMap["command"] = commandArr.first.chomp
+            # store the second element as the listName (if applicable)
+            @commandMap["listName"] = commandArr.length >= 2 ? commandArr[1].chomp : ""
+            # store the third element as the args (if applicable)
+            @commandMap["args"] = commandArr.length >= 3 ? commandArr[2].chomp : ""
+    end
+    def _undeclare
+        @listMap.delete(@commandMap["listName"])
+        return true
+    end
+    def _declare
+        @listMap[@commandMap["listName"]] = @commandMap["args"].length >= 1 ? @commandMap["args"].split(",") : []
+    end
+    def doesListExist()
+        puts "HELLOOOO"
+        if (@commandMap["listName"] == "" || !@listMap.key?(@commandMap["listName"]))
+            return false
+        end
       return true
     end
-    def upcase
+    def _upcase
       @IS_UPCASE ? @IS_UPCASE = false : @IS_UPCASE = true
+      return true
+#      puts "UPCASE VALUE: " + (@IS_UPCASE ? "true" : "false")
     end
-    def add(arr)
-      puts "hello from the add function..."
-      arr.each {|elem| puts elem}
+    def _list
+        @listMap.each {|key, value| printElem("#{key}", value)}
     end
+    def _print
+        return printElem(@commandMap["listName"], @listMap[@commandMap["listName"]])
+    end
+    
+def _size
+       puts "Size of " << @commandMap["listName"] << " is " << @listMap[@commandMap["listName"]].length.to_s
+        return true
+    end
+    def printElem(key, val)
+        key << ": "
+        print key
+        print @IS_UPCASE ? val.map(&:upcase) : val
+        return true
+    end
+    def _add
+        @listMap[@commandMap["listName"]] = @listMap[@commandMap["listName"]].concat @commandMap["args"].split(",")
+    end
+
     def verifyCommand(arg)
       puts "verifing: " + arg
       return true unless ! @validCommands.include?(arg)
@@ -70,18 +102,28 @@ class ListInterpreter
     def clearCommand
       @command = ""
     end
+    def clearMaps
+      @commandMap.clear
+      @listMap.clear
+    end
 end
+
+
+
 
 
 def main
   listInt = ListInterpreter.new()
-  listInt.setCommand("undeclare LISTNAME THIS, IS, A, LIST, OF, ARGS.....")
-  puts listInt.getCommand()
-  if (listInt.execute)
-    puts "its it is a valid command!"
-  else 
-    puts "this is invalid..."
+  print "> "
+
+  while line = STDIN.readline.chomp
+    listInt.setCommand(line)
+    if (!listInt.execute())
+      puts "there was an error..."
+    end
+    print "> "
   end
+
 end
 
 # call main
