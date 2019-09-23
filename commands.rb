31d9e1fe1e33fdd7a07624786d8cd99e130e5491
vp <vpatel@localhost.localdomain>
2019-09-22 18:01:30 -0700

    cleanup, comments, finished adding required functionality

--- commands.rb
+++ commands.rb
@@ -1,5 +1,40 @@
+#################################
+# Programmer: Vishal Patel
+# Date: 09/22/2019
+#################################
+
+# NOTE: 
+# ruby --version
+# ruby 2.5.5p157 (2019-03-15 revision 67260) [x86_64-linux]
+
+# ListInterpreter class
+# 	This class has the following members:
+# 		- IS_UPCASE 
+# 			- will be used to determine if the outputs should be in uppercase or not
+# 		- @command
+# 			- the entire command entered by the user will be stored in this variable (mostly used for debugging)
+# 		- validCommands
+# 			- an array of all of the valid commands specified in the assignment description
+# 		- commandMap
+# 			- used to parse the string 'command' into a map with the following keys:
+# 				- command
+# 				- listName
+# 				- args
+# 		- listMap
+# 			- used to store all of the declared lists with a key, value pair
+#
+# 	This class has the following public functions:
+# 		- getCommand
+# 			- returns the command as a string (used for debugging)
+# 		- setCommand
+# 			- stores the command entered by the user
+# 		- execute
+# 			- populates the commandMap with the appropriate keys
+# 			- verifies if the command is contained within the member validCommands
+# 			- checks if the list name is valid, whether it exists or not when applicable
+# 			- calls the appropriate function based on the command
+# 			- returns true of the command was executed sucessfully
 class ListInterpreter
-  # public...
   def initialize()
     @IS_UPCASE = false
     @command = ""
@@ -9,17 +44,22 @@ class ListInterpreter
     @commandMap = Hash.new
     @listMap = Hash.new
   end
+
+  # public functions...
   def getCommand
     return @command
   end
+
   def setCommand(newCommand)
     @command = newCommand
   end
+
   def execute
     # create a hashmap with the elements in the command string
     populateHashMap()
     # check if the command provided is a valid one
     if ! verifyCommand(@commandMap["command"])
+      puts "Command not found " + @commandMap["command"]
       return false
     end
 
@@ -31,19 +71,17 @@ class ListInterpreter
     end 
     # call the appropriate function, return its value (we already checked if it is a valid command earlier with verifyCommand...)
     if send("_" << @commandMap["command"])
-        clearCommand()
         return true
     end
     return false
-
   end
+
   # private functions...
   private
+    # populateHashMap will take the command string and parse it, putting elements into a commandMap with a key,value pair
     def populateHashMap()
             # split the command into an array first, at most three elements...
             commandArr = @command.split(' ', 3)
-            puts "LENGTH "
-            puts commandArr.length
             # store the first element as the command
             @commandMap["command"] = commandArr.first.chomp
             # store the second element as the listName (if applicable)
@@ -51,121 +89,170 @@ class ListInterpreter
             # store the third element as the args (if applicable)
             @commandMap["args"] = commandArr.length >= 3 ? commandArr[2].chomp : ""
     end
+
+    # isValidListName checks if the list exists or not depending on the stored command
     def isValidListName()
 	if (@commandMap["command"] =="declare" && doesListExist())
 	    # the list already exists... cannot declare it again
-	    puts "list name already exists..."
+	    puts "Error: List \"" + @commandMap["listName"] + "\" already exists"
 	    return false
 	end
 	if (((!(@commandMap["command"] == "declare")) && !doesListExist()) && @commandMap["command"] != "upcase" && @commandMap["command"] != "list")
-	    # not upcase, list, nor clear.  the list name does not exist...
-	    puts "list name does not exist..."
+	    # not declare, upcase, list, nor clear.  the list name does not exist...
+	    puts "Error: List \"" + @commandMap["listName"] + "\" doesn't exist"
 	    return false
 	end
 	# if the listName is valid
-	puts "VALID LIST NAME"
 	return true
     end
+
+    # undecare will undeclare the listname that is specified by the user
     def _undeclare
-        @listMap.delete(@commandMap["listName"])
+        result = @listMap.delete(@commandMap["listName"])
+        if result != nil
+	    puts "Removed " + @commandMap["listName"]
+	end
         return true
     end
+
+    # declare will create a list in listMap with the name specified by the user
     def _declare
-        @listMap[@commandMap["listName"]] = @commandMap["args"].length >= 1 ? @commandMap["args"].split(",") : []
+        @listMap[@commandMap["listName"]] = @commandMap["args"].length >= 1 ? @commandMap["args"].split(",").map(&:strip) : []
 	puts "Created " + @commandMap["listName"] + ": " + @listMap[@commandMap["listName"]].inspect
 	return true
     end
+
+    # doesListExist checks if a list exists in listMap
     def doesListExist()
         if (@commandMap["listName"] == "" || !@listMap.key?(@commandMap["listName"]))
             return false
         end
       return true
     end
+
+    # upcase toggles IS_UPCASE between true and false
     def _upcase
       @IS_UPCASE ? @IS_UPCASE = false : @IS_UPCASE = true
       return true
-#      puts "UPCASE VALUE: " + (@IS_UPCASE ? "true" : "false")
     end
+
+    # list will print each list in listMap
     def _list
-        @listMap.each {|key, value| printElem("#{key}", value)}
+        @listMap.each {|key, value| printPair("#{key}", value)}
     end
+
+    # print will print a specfied list
     def _print
-        return printElem(@commandMap["listName"], @listMap[@commandMap["listName"]])
+        return printPair(@commandMap["listName"], @listMap[@commandMap["listName"]])
     end
-    
+    # size will print the size of a specified list
     def _size
        puts "Size of " << @commandMap["listName"] << " is " << @listMap[@commandMap["listName"]].length.to_s
         return true
     end
+    # clear will clear the contents of a specific list
     def _clear
 	@listMap[@commandMap["listName"]] = []
+	puts "Cleared " + @commandMap["listName"]
 	return true
     end
-    def printElem(key, val)
+    # printPair will print a list in a format depending on IS_UPCASE
+    def printPair(key, val)
         key << ": "
         print key
-        print @IS_UPCASE ? val.map(&:upcase) : val
-	puts ""	
+        printValue(val)
         return true
     end
-    def _add
-        @listMap[@commandMap["listName"]] = @listMap[@commandMap["listName"]].concat @commandMap["args"].split(",")
+    def printValue(val)
+	print @IS_UPCASE ? val.map(&:upcase) : val
+	puts ""	
     end
+
+    # add will add elements to a list
+    def _add
+        @listMap[@commandMap["listName"]] = @listMap[@commandMap["listName"]].concat @commandMap["args"].split(",").map(&:strip)
+	outputString = "Added "
+	@commandMap["args"].split(",").each {|elem| outputString = outputString + "\"" + elem + "\" " }
+	outputString += "to the list"
+	puts outputString
+	return true
+    end
+
+    # remove will remove elements from a list
     def _remove
 	index = @listMap[@commandMap["listName"]].index(@commandMap["args"])
 	if (index != nil)
 		@listMap[@commandMap["listName"]].delete_at(index)
+		puts "Removed index " + index.to_s + " of " + @commandMap["listName"]
 	end
 	return true
     end
+
+    # test_1 will print elements of a list that does not end with the character 'e'
     def _test_1
-	puts @listMap[@commandMap["listName"]].select{|elem| elem[-1] == "e"}.inspect
+	outputString = "test_1 on " + @commandMap["listName"] + ": "
+	print outputString
+ 	printValue(@listMap[@commandMap["listName"]].select{|elem| elem[-1] != "e"})
 	return true
     end
+
+    # test_2 will print elements of a list that is greater than 3 but less than 6, or is greater than 7
     def _test_2
-        puts @listMap[@commandMap["listName"]].select{|elem| ((elem.length > 3 && elem.length < 6) || elem.length > 7)}.inspect
-        return true
-    end
-    def _test_3
-        puts "NOT YET IMPLMENTED...."
-	return true
-    end
-    def _test_4
-        puts @listMap[@commandMap["listName"]].select{ |elem| elem.include? @commandMap["args"]}.inspect
+	outputString = "test_2 on " + @commandMap["listName"] + ": "
+        print outputString
+	printValue(@listMap[@commandMap["listName"]].select{|elem| ((elem.length > 3 && elem.length < 6) || elem.length > 7)})
         return true
     end
 
+    def _test_3
+        outputString = "test_3 on " + @commandMap["listName"] + ": "
+        print outputString
+	printValue(@listMap[@commandMap["listName"]].select{|elem| (elem.length > 8)}[0...5])
+	return true
+    end
+
+    # test_4 will print elements of a list that contain a substring defined in @commandMap[args]
+    def _test_4
+	outputString = "test_4 on " + @commandMap["listName"] + ": "
+        print outputString
+        printValue(@listMap[@commandMap["listName"]].select{ |elem| elem.include? @commandMap["args"]})
+        return true
+
+    end
+    # verifyCommand will check if a command is recognized 
     def verifyCommand(arg)
-      puts "verifing: " + arg
       return true unless ! @validCommands.include?(arg)
       return false
     end
-    def clearCommand
-      @command = ""
-    end
-    def clearMaps
-      @commandMap.clear
-      @listMap.clear
-    end
-end
+end # end ListInterpreter
 
 
 
 
 
 def main
+  # create instance of ListInterpreter
   listInt = ListInterpreter.new()
+  # print the prompt
   print "> "
 
+  # infinite while loop
   while line = STDIN.readline.chomp
+    # break out of the loop if an empty string is detected
+    break unless line != ""
+    # set the command to the string contained in 'line'
     listInt.setCommand(line)
+    # attempt to execute the command
     if (!listInt.execute())
+      # inform user that there was an error (another print would be printed above here if there really was an error as well)
       puts "there was an error..."
     end
+    # prompt the user for the next command:w
     print "> "
-  end
+  end # end while
 
-end
+  puts "Bye"
+end # end main
 
 # call main
 main
